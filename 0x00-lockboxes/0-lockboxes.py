dd6a37bc4ab6e80643ade087d71a3ab1bafba3c9
blanketmanatee <ashleyl.welch@gmail.com>
2021-07-22 23:19:51 -0500

    more fixes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-    ''' can all boxes be unlocked '''
-    def canUnlockAll(boxes)
-        ''' create a list of keys '''
-        seen = [False] * len(boxes)
-        seen[0] = True
-        stack = [0]
-        while stack:  #While we have keys...
-            node = stack.pop() # get the next key 'node'
-            for nei in boxes[node]: # For every key in box # 'node'...
-                if not seen[nei]: # ... that hasn't been used yet
-                    seen[nei] = True # mark that we've opened the box
-                    stack.append(nei) # add the key to the todo list
-        return all(seen) # Return true if we've visited every box
+''' can all boxes be unlocked '''
+    
+def canUnlockAll(boxes)
+    ''' create a list of keys '''
+    seen = [False] * len(boxes)
+    seen[0] = True
+    stack = [0]
+    while stack:  #While we have keys...
+        node = stack.pop() # get the next key 'node'
+        for nei in boxes[node]: # For every key in box # 'node'...
+            if not seen[nei]: # ... that hasn't been used yet
+                seen[nei] = True # mark that we've opened the box
+                stack.append(nei) # add the key to the todo list
+    return all(seen) # Return true if we've visited every box

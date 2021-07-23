f2df6ba090eaa09d89455dbbeeaeed54b8836194
blanketmanatee <ashleyl.welch@gmail.com>
2021-07-22 23:25:16 -0500

    comment formatting

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -13,4 +13,5 @@ def canUnlockAll(boxes):
             if not seen[nei]:  #  ... that hasn't been used yet
                 seen[nei] = True  #  mark that we've opened the box
                 stack.append(nei)  #  add the key to the todo list
-    return all(seen)  ''' Return true if we've visited every box'''
+    return all(seen)  
+    ''' Return true if we've visited every box'''

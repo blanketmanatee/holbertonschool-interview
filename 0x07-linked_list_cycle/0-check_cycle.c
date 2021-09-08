37ab3b85d3a1ad52d64b0499aab09692f0dab26d
blanketmanatee <ashleyl.welch@gmail.com>
2021-09-08 18:59:09 -0500

    initial attempt

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check if a singlly linked list has a cycle
+ * @list: pointer to list head
+ * Return: 0 if no cycle, or 1
+ */
+
+int cyeck_cycle(listint_t *list)
+{
+    listint_t *first, *second;
+
+    if (list == NULL)
+            return (0);
+    if (list->next == NULL)
+            return (0);
+        first = list;
+        second = list->next;
+        do {
+            if (first == second)
+                return (1);
+            first = first->next;
+            if (second->next != NULL)
+                second = second->next->next;
+            else
+                second = NULL;
+        } while (first != NULL && second != NULL);
+        return (0);
+}

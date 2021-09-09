34d7d1d45a70294265e34b8c955ab27b003d9771
blanketmanatee <ashleyl.welch@gmail.com>
2021-09-08 19:24:43 -0500

    betty

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -9,17 +9,17 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) 
 {
-    heap_t *node;
+heap_t *node;
 
-    node = malloc(sizeof(heap_t));
-    if (node == NULL)
-            return (NULL);
+node = malloc(sizeof(heap_t));
+if (node == NULL)
+return (NULL);
 
-    node->n = value;
-    node->parent = parent;
-    node->left = NULL;
-    node->right = NULL;
+node->n = value;
+node->parent = parent;
+node->left = NULL;
+node->right = NULL;
 
-    return (node);
+return (node);
 }
 

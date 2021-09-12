a7a1d8676fb17448c58a096814da70eda85b40c0
blanketmanatee <ashleyl.welch@gmail.com>
2021-09-11 22:30:22 -0500

    initial commit

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,14 @@
+#include "sandpiles.h"
+
+/**
+ * sandpiles_sum - computes the sum of two sandpiles
+ * @grid1: stable
+ * @grid2: stable
+ * Return: void
+ */
+
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+    
+}

385fd3049eb0fcb02440c1dd8a01f689449243db
blanketmanatee <ashleyl.welch@gmail.com>
2021-10-07 18:30:25 -0500

    initial attempt

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -6,5 +6,27 @@
  */
 
 void menger(int level) {
-    
+    int i, j, x, y, result;
+    char shape;
+
+    result = pow(3, level);
+
+    for (i = 0; i < result; i++)
+    {
+        for (j = 0; j < result; j++)
+        {
+            shape = '#';
+            x = i;
+            y = j;
+            while (x > 0)
+            {
+                if (x % 3 == 1 && y % 3 == 1)
+                shape = " ";
+                x /= 3;
+                y /= 3;
+            }
+            printf("%c", shape);
+        }
+        printf("/n");
+    }
 }

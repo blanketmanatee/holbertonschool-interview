cabc7a0793c1b9582a2bdda30d9c7d51c2bb5646
blanketmanatee <ashleyl.welch@gmail.com>
2021-10-07 20:18:29 -0500

    maybe?

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -11,19 +11,29 @@ void menger(int level) {
 
     result = pow(3, level);
 
+    if (level < 0)
+        return;
+
+    if (level == 0)
+    {
+        printf("\n");
+        return;
+    }
+
     for (i = 0; i < result; i++)
     {
         for (j = 0; j < result; j++)
         {
-            shape = '#';
             x = i;
             y = j;
-            while (x > 0)
+            shape = '#';
+
+            while (x)
             {
                 if (x % 3 == 1 && y % 3 == 1)
-                shape = " ";
-                x /= 3;
-                y /= 3;
+                shape = ' ';
+                x = x / 3;
+                y = y / 3;
             }
             printf("%c", shape);
         }

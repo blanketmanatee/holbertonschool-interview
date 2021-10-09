46160f195ab79a2b62c0ec735b6b2aa1102ce648
blanketmanatee <ashleyl.welch@gmail.com>
2021-10-08 22:45:46 -0500

    betty

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -9,22 +9,22 @@
 
 heap_t *heap_insert(heap_t **root, int value)
 {
-    heap_t *node;
+heap_t *node;
 
-    if (*root == NULL)
-    {
-        *root = binary_tree_node(NULL, value);
-        return (*root);
-    }
-    node = insert(*root, value, 0, tree_size(*root) + 1);
-    while (node->parent && node->n > node->parent->n)
-    {
-        node->n = node->parent->n;
-        node->parent->n = value;
-        node = node->parent;
-    }
+if (*root == NULL)
+{
+*root = binary_tree_node(NULL, value);
+return (*root);
+}
+node = insert(*root, value, 0, tree_size(*root) + 1);
+while (node->parent && node->n > node->parent->n)
+{
+node->n = node->parent->n;
+node->parent->n = value;
+node = node->parent;
+}
 
-    return(node);
+return(node);
 }
 
 /**
@@ -35,20 +35,20 @@ heap_t *heap_insert(heap_t **root, int value)
 
 size_t tree_size(const binary_tree_t *tree)
 {
-    size_t size = 0, right = 0, left = 0;
+size_t size = 0, right = 0, left = 0;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    else
-    {
-        left = tree_size(tree->left);
-        right = tree_size(tree->right);
-        size = right + left + 1;
-    }
+if (tree == NULL)
+{
+return (0);
+}
+else
+{
+left = tree_size(tree->left);
+right = tree_size(tree->right);
+size = right + left + 1;
+}
 
-    return (size);
+return (size);
 }
 
 /**
@@ -62,32 +62,32 @@ size_t tree_size(const binary_tree_t *tree)
 
 heap_t *insert(binary_tree_t *tree, int value, int index, int leaf)
 {
-    heap_t *node;
+heap_t *node;
 
-    if (tree == NULL)
-    {
-        return (NULL);
-    }
-    if (index + 1 == leaf / 2)
-    {
-        if (leaf & 1)
-        {
-            tree->right = binary_tree_node(tree, value);
-            return(tree->right);
-        }
-        else
-        {
-            tree->left = binary_tree_node(tree, value);
-            return (tree->left);
-        }
-    }
-    node = insert(tree->left, value, 2 * index + 1, leaf);
-    if (node != 0)
-    {
-        return (node);
-    }
-    else
-    {
-        return (insert(tree->right, value, 2 * index + 2, leaf));
-    }
+if (tree == NULL)
+{
+return (NULL);
+}
+if (index + 1 == leaf / 2)
+{
+if (leaf & 1)
+{
+tree->right = binary_tree_node(tree, value);
+return(tree->right);
+}
+else
+{
+tree->left = binary_tree_node(tree, value);
+return (tree->left);
+}
+}
+node = insert(tree->left, value, 2 * index + 1, leaf);
+if (node != 0)
+{
+return (node);
+}
+else
+{
+return (insert(tree->right, value, 2 * index + 2, leaf));
+}
 }

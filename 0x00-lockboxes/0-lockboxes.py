672ff7c06313204947e5a8ce4e01390abcfdde8d
blanketmanatee <ashleyl.welch@gmail.com>
2021-10-08 21:35:45 -0500

    reworking it

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,20 +4,17 @@
 
 def canUnlockAll(boxes):
     ''' create a list of keys '''
-    seen = [False] * len(boxes)
-    seen[0] = True
-    stack = [0]
-    while stack:
-        ''' While we have keys...'''
-        node = stack.pop()
-        '''  get the next key 'node' '''
-        for key in boxes[node]:
-            ''' For every key in box # 'node'...'''
-            if not seen[key]:
-                '''  ... that hasn't been used yet'''
-                seen[key] = True
-                '''  mark that we've opened the box'''
-                stack.append(key)
-                '''add the key to the todo list'''
-    return all(seen)
-    ''' Return true if we've visited every box'''
+    if type(boxes) is not list:
+       return False
+    elif (len(boxes)) == 0:
+        return False
+    for k in range(1, len(boxes) -1):
+        seen = False
+        for idx in range(len(boxes)):
+            seen = k in boxes[idx] and k != idx
+            if seen:
+                break
+        if seen is False:
+            return seen
+    return True
+            

565c1dc16ffc2c1a322b3ee77aa82d4b698b2579
blanketmanatee <ashleyl.welch@gmail.com>
2021-10-09 16:05:10 -0500

    checker

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -7,37 +7,45 @@ if __name__ == "__main__":
     import signal
 
     file_size = [0]
-    count = 1
-    statCount = {"200": 0, "301": 0, "400": 0, "401": 0,
-                 "403": 0, "404": 0, "405": 0, "500": 0}
+    codes = [200, 301, 400, 401, 403, 404, 405, 500]
+    count = 0
+    size = 0
+    statCount = {'200': 0, '301': 0, '400': 0, '401': 0,
+                 '403': 0, '404': 0, '405': 0, '500': 0}
 
-    def parseIt():
-        print("File size: {}".format(file_size[0]))
-        for key in sorted(statCount.keys()):
-            if statCount[key] != 0:
+    try: 
+        for ln in sys.stdin:
+            if count == 10:
+                print("File size: {}".format(file_size))
+            for key in sorted(statCount):
                 print("{}: {}".format(key, statCount[key]))
+            count = 1
+        else: count += 1
 
-    def stdin_parse(line):
-        """
-        checks stdin
-        """
+        ln = ln.split()
+
         try:
-            line = line[:-1]
-            word = line.split(' ')
-            file_size[0] += int(word[-1])
-            status_code = int(word[-2])
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-        except BaseException:
+            size = size + int(ln[-1])
+        except (IndexError, ValueError):
             pass
 
-    try:
-        for line in sys.stdin:
-            stdin_parse(line)
-            if count % 10 == 0:
-                parseIt()
-            count += 1
+        try:
+            if ln[-2] in statCount:
+                if codes.get(ln[-2], -1) == -1:
+                    codes[ln[-2]] = 1
+                else:
+                    codes[ln[-2]] += 1
+        except IndexError:
+            pass
+
+        print("File size: {}".format(file_size))
+        for key in sorted(codes):
+            print("{}: {}".format(key, codes[key]))
+
+
+    
     except KeyboardInterrupt:
-        parseIt()
+        print("File size:{}".format(file_size))
+        for key in sorted(statCount):
+            print("{}: {}".format(key, statCount[key]))
         raise
-    parseIt()

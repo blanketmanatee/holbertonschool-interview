2fcb6c4689b9ff8bb3708a7e274e83d60d56f2fc
blanketmanatee <ashleyl.welch@gmail.com>
2021-10-08 22:58:11 -0500

    betty

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -6,19 +6,19 @@
  **/
 static void print_grid(int grid[3][3])
 {
-    int x, y;
+int x, y;
 
-    printf("=\n");
-    for (x = 0; x < 3; x++)
-    {
-        for (y = 0; y < 3; y++)
-        {
-            if (y)
-            printf(" ");
-            printf("%d", grid[x][y]);
-        }
-        printf("\n");
-    }
+printf("=\n");
+for (x = 0; x < 3; x++)
+ {
+for (y = 0; y < 3; y++)
+{
+if (y)
+printf(" ");
+printf("%d", grid[x][y]);
+}
+printf("\n");
+ }
 }
 
 /**
@@ -31,35 +31,35 @@ static void print_grid(int grid[3][3])
 
 int topples(int grid[3][3], int x, int y)
 {
-    int unstable = 0;
+int unstable = 0;
 
-    grid[x][y] -= 4;
-    if (x + 1 >= 0 && x + 1 <= 2)
-    {
-        grid[x + 1][y]++;
-        if (grid[x + 1][y] > 3)
-        unstable++;
+grid[x][y] -= 4;
+if (x + 1 >= 0 && x + 1 <= 2)
+{
+grid[x + 1][y]++;
+if (grid[x + 1][y] > 3)
+unstable++;
 
-    }
-    if (y + 1 >= 0 && y + 1 <= 2)
-    {
-        grid[x][y + 1]++;
-        if (grid[x][y + 1] > 3)
-        unstable++;
-    }
-    if (x - 1 >= 0 && y - 1 <= 2)
-    {
-        grid[x - 1][y]++;
-        if (grid[x - 1][x] > 3)
-        unstable++;
-    }
-    if (y - 1 >= 0 && y - 1 <= 2)
-    {
-        grid[x][y - 1]++;
-        if (grid[x][y - 1] > 3)
-        unstable++;
-    }
-    return (unstable);
+}
+if (y + 1 >= 0 && y + 1 <= 2)
+{
+grid[x][y + 1]++;
+if (grid[x][y + 1] > 3)
+unstable++;
+}
+if (x - 1 >= 0 && y - 1 <= 2)
+{
+grid[x - 1][y]++;
+if (grid[x - 1][x] > 3)
+unstable++;
+}
+if (y - 1 >= 0 && y - 1 <= 2)
+{
+grid[x][y - 1]++;
+if (grid[x][y - 1] > 3)
+unstable++;
+}
+return (unstable);
 }
 
 /**
@@ -69,33 +69,33 @@ int topples(int grid[3][3], int x, int y)
 
 void loop_unstable(int grid [3][3])
 {
-    int x, y, unstable = 1, t = 0;
-    int copy[3][3];
+int x, y, unstable = 1, t = 0;
+int copy[3][3];
 
-    while (unstable)
-    {
-        for (x = 0; x < 3; x++)
-        {
-            for (y = 0; y < 3; y++)
-            {
-                copy[x][y] = grid [x][y];
-            }
-        }
-        for (x = 0; x < 3; x++)
-        {
-            for (y = 0; y < 3; y++)
-            {
-                if (copy[x][y] > 3)
-                {
-                    t = topples(grid, x, y);
-                }
-            }
-        }
-        if (!t || (!t && x == 0))
-        unstable = 0;
-        else
-        print_grid(grid);
-    }
+while (unstable)
+{
+for (x = 0; x < 3; x++)
+{
+for (y = 0; y < 3; y++)
+{
+copy[x][y] = grid [x][y];
+}
+}
+for (x = 0; x < 3; x++)
+{
+for (y = 0; y < 3; y++)
+{
+if (copy[x][y] > 3)
+{
+t = topples(grid, x, y);
+}
+}
+}
+if (!t || (!t && x == 0))
+unstable = 0;
+else
+print_grid(grid);
+}
 }
 /**
  * sandpiles_sum - computes the sum of two sandpiles
@@ -107,26 +107,26 @@ void loop_unstable(int grid [3][3])
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int x, y, temp, over = 0;
+int x, y, temp, over = 0;
 
-    for (x = 0; x < 3; x++)
-    {
-        for (y = 0; y < 3; y++)
-        {
-            temp = grid1[x][y];
-            grid1[x][y] = temp + grid2[x][y];
-            if (grid1[x][y] > 3)
-            {
-                over = 1;
-            }
-        }
-    }
-    if (over)
-    {
-        print_grid(grid1);
-        loop_unstable(grid1);
-    }
-    else
-    loop_unstable(grid1);
+for (x = 0; x < 3; x++)
+{
+for (y = 0; y < 3; y++)
+{
+temp = grid1[x][y];
+grid1[x][y] = temp + grid2[x][y];
+if (grid1[x][y] > 3)
+{
+over = 1;
+}
+}
+}
+if (over)
+{
+print_grid(grid1);
+loop_unstable(grid1);
+}
+else
+loop_unstable(grid1);
     
 }

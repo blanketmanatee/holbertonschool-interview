541c5842f2a32705e64a430e2e5e16fe6f8165dc
blanketmanatee <ashleyl.welch@gmail.com>
2021-10-08 22:40:31 -0500

    adding task 1

---
+++ 0x02-heap_insert/1-heap_insert.c
@@ -0,0 +1,93 @@
+#include "binary_trees.h"
+
+/**
+ * heap_insert - inserts a value into a max binary heap
+ * @root: double pointer to root node
+ * @value: value stored in node to be inserted
+ * Return: pointer to new node
+ */
+
+heap_t *heap_insert(heap_t **root, int value)
+{
+    heap_t *node;
+
+    if (*root == NULL)
+    {
+        *root = binary_tree_node(NULL, value);
+        return (*root);
+    }
+    node = insert(*root, value, 0, tree_size(*root) + 1);
+    while (node->parent && node->n > node->parent->n)
+    {
+        node->n = node->parent->n;
+        node->parent->n = value;
+        node = node->parent;
+    }
+
+    return(node);
+}
+
+/**
+ * size - function to find size of the tree
+ * @tree: tree
+ * Return: size of tree
+ */
+
+size_t tree_size(const binary_tree_t *tree)
+{
+    size_t size = 0, right = 0, left = 0;
+
+    if (tree == NULL)
+    {
+        return (0);
+    }
+    else
+    {
+        left = tree_size(tree->left);
+        right = tree_size(tree->right);
+        size = right + left + 1;
+    }
+
+    return (size);
+}
+
+/**
+ * insert - inserts node
+ * @tree: ptr
+ * @value: value of the node
+ * @index: index of the node
+ * @lead: node array position
+ * Return: node inserted
+ */
+
+heap_t *insert(binary_tree_t *tree, int value, int index, int leaf)
+{
+    heap_t *node;
+
+    if (tree == NULL)
+    {
+        return (NULL);
+    }
+    if (index + 1 == leaf / 2)
+    {
+        if (leaf & 1)
+        {
+            tree->right = binary_tree_node(tree, value);
+            return(tree->right);
+        }
+        else
+        {
+            tree->left = binary_tree_node(tree, value);
+            return (tree->left);
+        }
+    }
+    node = insert(tree->left, value, 2 * index + 1, leaf);
+    if (node != 0)
+    {
+        return (node);
+    }
+    else
+    {
+        return (insert(tree->right, value, 2 * index + 2, leaf));
+    }
+}
--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -29,5 +29,9 @@ void binary_tree_print(const binary_tree_t *);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 heap_t *heap_insert(heap_t **root, int value);
+heap_t *insert(binary_tree_t *tree, int value, int index, int leaf);
+size_t tree_size(const binary_tree_t *tree);
+typedef struct binary_tree_s heap_t;
+size_t tree_size(const binary_tree_t *tree);
 
 #endif /* BINARYTREES */

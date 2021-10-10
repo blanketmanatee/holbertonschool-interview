3183395ea2aba7e835774c20a06ec6c48158c32f
blanketmanatee <ashleyl.welch@gmail.com>
2021-10-10 04:16:27 -0500

    betty

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -5,38 +5,39 @@
  * @level: level of sponge to draw
  */
 
-void menger(int level) {
-    int i, j, x, y, result;
-    char shape;
+void menger(int level) 
+{
+int i, j, x, y, result;
+char shape;
 
-    result = pow(3, level);
+result = pow(3, level);
 
-    if (level < 0)
-        return;
+if (level < 0)
+return;
 
-    if (level == 0)
-    {
-        printf("\n");
-        return;
-    }
+if (level == 0)
+{
+printf("\n");
+return;
+}
 
-    for (i = 0; i < result; i++)
-    {
-        for (j = 0; j < result; j++)
-        {
-            x = i;
-            y = j;
-            shape = '#';
+for (i = 0; i < result; i++)
+{
+for (j = 0; j < result; j++)
+{
+x = i;
+y = j;
+shape = '#';
 
-            while (x)
-            {
-                if (x % 3 == 1 && y % 3 == 1)
-                shape = ' ';
-                x = x / 3;
-                y = y / 3;
-            }
-            printf("%c", shape);
-        }
-        printf("\n");
-    }
+while (x)
+{
+if (x % 3 == 1 && y % 3 == 1)
+shape = ' ';
+x = x / 3;
+y = y / 3;
+}
+printf("%c", shape);
+}
+printf("\n");
+}
 }

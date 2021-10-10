cdc631facbe564d0a06dc157559e1f2c6b613691
blanketmanatee <ashleyl.welch@gmail.com>
2021-10-09 20:58:29 -0500

    none

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -14,20 +14,21 @@ def validUTF8(data):
 
     n_bytes = 0
 
-    for num in data:
-        bin_rep = format(num, '#010b')[-8:]
+    a = 1 << 7
+    b = 1 << 6
 
+    for num in data:
+        a = 1 << 7
         if n_bytes == 0:
-            for bit in bin_rep:
-                if bit == '0':
-                    break
+            while a & num:
                 n_bytes += 1
+                a = a >> 1
+            if n_bytes == 0:
+                continue
             if n_bytes == 1 or n_bytes > 4:
                 return False
-    else:
-        if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
-            return False
-
+        else:
+            if not (num and a and not (num & b)):
+                return False
             n_bytes -= 1
-
-            return n_bytes == 0
+        return n_bytes == 0

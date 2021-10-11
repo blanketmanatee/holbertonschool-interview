d53bba249b120d903df9d1c9ce29369cba3a03f8
blanketmanatee <ashleyl.welch@gmail.com>
2021-10-11 06:43:05 -0500

    checker

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -14,21 +14,21 @@ def validUTF8(data):
 
     n_bytes = 0
 
-    a = 1 << 7
-    b = 1 << 6
+    m1 = 1 << 7
+    m2 = 1 << 6
 
     for num in data:
-        a = 1 << 7
+        m = 1 << 7
         if n_bytes == 0:
-            while a & num:
+            while m & num:
                 n_bytes += 1
-                a = a >> 1
+                m = m >> 1
             if n_bytes == 0:
                 continue
             if n_bytes == 1 or n_bytes > 4:
                 return False
         else:
-            if not (num and a and not (num & b)):
+            if not (num and m1 and not (num & m2)):
                 return False
             n_bytes -= 1
         return n_bytes == 0

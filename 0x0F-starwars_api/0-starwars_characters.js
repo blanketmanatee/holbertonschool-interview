618cdcc0b59295138401fda7653f4cd3e0b7b3ae
blanketmanatee <ashleyl.welch@gmail.com>
2021-11-12 09:18:07 -0600

    done

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+const urlApi = 'https://swapi-api.hbtn.io/'
+const movieId = process.argv[2];
+
+request(urlApi + movieId, (error, response, body) => {
+    if (error) throw error;
+    const characters = JSON.parse(body).characters;
+    showNames(characters);
+});
+const showNames = (names, i = 0) => {
+    if (i === names.length) return;
+    request(names[i], (error, response, body) => {
+        if (error) throw error;
+        console.log(JSON.parse(body).name);
+        showNames(names, i + 1);
+    });
+};

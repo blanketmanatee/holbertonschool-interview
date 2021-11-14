6938d8917e5d9983d41fc3067f4c5f9470458a68
blanketmanatee <ashleyl.welch@gmail.com>
2021-11-13 22:46:40 -0600

    second attempt

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,23 +1,14 @@
 #!/usr/bin/node
 
 const request = require('request');
+const url = 'https://swapi-api.htbn.io/api/films/'.concat(process.argv[2]);
 
-const movieId = process.argv[2] + '/';
-const url = 'https://swapi-api.htbn.io/api/films/';
+request(url, function (err, res, body) {
+  const characters = JSON.parse(body).characters;
 
-request(url + movieId, async function (err, res, body) {
-  if (err) return console.error(err);
-
-  const charurl = JSON.parse(body).characters;
-
-  for (const char of charurl) {
-    await new Promise(function (resolve, reject) {
-      request(char, function (err, res, body) {
-        if (err) return console.error(err);
-
-        console.log(JSON.parse(body).name);
-        resolve();
-      });
+  for (let i = 0; i < characters.length; ++i) {
+    request(characters[i], function (err, res, body) {
+      console.log(JSON.parse(body).name);
     });
   }
 });

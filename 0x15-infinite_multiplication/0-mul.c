00379f71e125968dc2a76375d212c1b6d0772ced
blanketmanatee <ashleyl.welch@gmail.com>
2021-12-22 14:47:53 -0600

    c

---
+++ 0x15-infinite_multiplication/0-mul.c
@@ -0,0 +1,97 @@
+#include "holberton.h"
+
+/**
+ * is_digit - checks that arg is a number
+ * @x: arg
+ * Return: 0 on success
+ */
+
+int is_digit(char *x)
+{
+    while (*x)
+    {
+        if (*x < '0' || *x > '9')
+        return (0);
+
+        x++;
+    }
+    return (1);
+}
+
+/**
+ * _strlen - find len of str 
+ * @s: str
+ * Return: len of str
+ */
+
+int _strlen(char *s)
+{
+    char *ptr = s;
+
+    while (*s)
+    s++;
+    return (s - ptr);
+}
+
+/**
+ * multiply - mul two numbers
+ * @a: num
+ * @b: num
+ */
+
+void multiply(char *a, char *b)
+{
+    int i, len_a, len_b, total, a_num, b_num, res = 0, tmp;
+    int *ptr;
+
+    len_a = _strlen(a);
+    len_b = _strlen(b);
+    tmp = len_b;
+    total = len_a + len_b;
+    ptr = malloc(sizeof(int) * total);
+    if (!ptr)
+    return;
+    for (len_a--; len_a >= 0; len_a--)
+    {
+        a_num = a[len_a] - '0';
+        res = 0;
+        len_b = tmp;
+        for (len_b--; len_b >= 0; len_b--)
+        {
+            b_num = b[len_b] - '0';
+            res += ptr[len_b + len_a + 1] + (a_num * b_num);
+            ptr [len_a + len_b  + 1] = res % 10;
+            res /= 10;
+        }
+        if (res)
+        ptr[len_a + len_b + 1] = res % 10;
+    }
+    while (*ptr == 0)
+    {
+        ptr++;
+        total--;
+    }
+    for (i = 0; i < total; i++)
+    printf("%i", ptr[i]);
+    printf("\n");
+}
+
+/**
+ * main - Entry point
+ * @argc: arg count
+ * @argv: arg values
+ * Return: 0 on success
+ **/
+int main(int argc, char *argv[])
+{
+char *num1 = argv[1];
+char *num2 = argv[2];
+
+if (argc != 3 || !is_digit(num1) || !is_digit(num2))
+{
+printf("Error\n");
+exit(98);
+}
+multiply(num1, num2);
+return (0);
+}
---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,14 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]);
+int _putchar(char c);
+int _strlen(char *);
+int is_digit(char *x);
+void multiply(char *s1, char *s2);
+
+#endif /* _HOLBERTON_H_ */

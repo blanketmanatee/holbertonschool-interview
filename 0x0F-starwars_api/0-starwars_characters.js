fcbe4f09a07663beca025be062bffab094be865a
blanketmanatee <ashleyl.welch@gmail.com>
2022-01-03 23:58:56 -0600

    fixing

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,14 +1,22 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi-api.htbn.io/api/people/'.concat(process.argv[2]);
+const filmId = process.argv[2];
+const url = 'https://swapi-api.htbn.io/api/films/${filmId}';
 
-request(url, function (err, res, body) {
-  const characters = JSON.parse(body).characters;
-
-  for (let i = 0; i < characters.length; ++i) {
-    request(characters[i], function (err, res, body) {
-      console.log(JSON.parse(body).name);
+request(url, async (err, res, body) {
+  if (err) {
+    console.log(err);
+  }
+  for (const characters = JSON.parse(body).characters){
+    await new Promise((resolve, reject) => {
+      request(characters, (err, res, body) => {
+        if (err) {
+          reject(err);
+        }
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
     });
   }
 });

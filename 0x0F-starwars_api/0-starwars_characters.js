d69fd815b9e4b4bca479e629dbad599fab7be463
blanketmanatee <ashleyl.welch@gmail.com>
2022-01-07 14:24:21 -0600

    new attempt

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -4,19 +4,22 @@ const request = require('request');
 const filmId = process.argv[2];
 const url = 'https://swapi-api.htbn.io/api/films/${filmId}';
 
-request(url, async (err, res, body) =>
-  { (err) =>
-        console.log(err);
-  }
-  for (const characters = JSON.parse(body).characters){
-    await new Promise((resolve, reject) => {
-      request(characters, (err, res, body) => {
-        if (err) {
-          reject(err);
-        }
-        console.log(JSON.parse(body).name);
-        resolve();
+request(url, async (err, res, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const allchar = JSON.parse(body).characters;
+    for (const char of allchar) {
+      const res = await new Promise((resolve, reject) => {
+        request(char, (error, res, body2) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(body2).name);
+          }
+        });
       });
-    });
+      console.log(res);
+    }
   }
 });

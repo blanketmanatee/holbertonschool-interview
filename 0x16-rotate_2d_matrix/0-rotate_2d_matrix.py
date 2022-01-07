9b8f619682b9182f6d2db50844d6a4e521824a42
blanketmanatee <ashleyl.welch@gmail.com>
2022-01-07 13:49:42 -0600

    docs

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+""" Rotate 2D matrix"""
 
 def rotate_2d_matrix(matrix):
+    """ rotate 2D matrix """
     for x in range(0, int(N / 2)):
         for y in range(x, N-x-1):
             temp = matrix[x][y]
@@ -10,6 +12,7 @@ def rotate_2d_matrix(matrix):
             matrix[N-1-y][x] = temp
 
             def displayMatrix( matrix ):
+                """show matrix"""
                 for i in range(0, N):
                     for j in range(0, N):
                         print (matrix[i][j], end = ' ')

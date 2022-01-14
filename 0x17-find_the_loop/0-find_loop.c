c807ffb3a4770135e11bbab9d2a6cd28c436c9dc
blanketmanatee <ashleyl.welch@gmail.com>
2022-01-13 18:10:57 -0600

    push

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * find_listint_loop - function to find loop in a linked list
+ * @head: ptr to linked list
+ * Return: address of node where the loop begins or NULL
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+listint_t *i = head;
+listint_t *j = head;
+
+if (!head)
+return (NULL);
+while(i->next && j->next->next)
+{
+i = i->next;
+j = j->next->next;
+if (i == j)
+{
+i = head;
+while (i != j)
+{
+i = i->next;
+j = j->next;
+}
+return (j);
+}
+}
+return (NULL);
+}
---
+++ 0x17-find_the_loop/README.md
@@ -0,0 +1,1 @@
+Write a function that finds the loop in a linked list.

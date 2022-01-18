cfa826cfdc11652e5ccc23f1b2e8edf5d09b47d4
blanketmanatee <ashleyl.welch@gmail.com>
2022-01-18 11:04:04 -0600

    done

--- 0x18-merge_sort/0-merge_sort.c
+++ 0x18-merge_sort/0-merge_sort.c
@@ -46,3 +46,41 @@ void merge(int *array, int size, int m, int *tmp)
     printf("[Done]: ");
     print_array(tmp, size);
 }
+
+/**
+ * merge_main - merge
+ * @array: ptr
+ * @size: size
+ * @tmp: ptr
+ */
+
+void merge_main(int *array, size_t size, int *tmp)
+{
+    int m;
+
+    if (size < 2)
+    return;
+    m = size / 2;
+    merge_main(array, m, tmp);
+    merge_main(array + m, size - m, tmp);
+    merge(array, size, m, tmp);
+}
+
+/**
+ * merge_sort - merge sort
+ * @array: ptr
+ * @size: size
+ */
+
+void merge_sort(int *array, size_t size)
+{
+    int *tmp = NULL;
+
+    if (!array || size < 2)
+    return;
+    tmp = malloc(sizeof(int) * size);
+    if (!tmp)
+    return;
+    merge_main(array, size, tmp);
+    free(tmp);
+}
